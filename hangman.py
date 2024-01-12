48ef9ee2ee71e86c72ba892fce300c68fcfb10a5
Rana <sahinra222@gmail.com>
2024-01-12 14:40:24 +0100

    Win/Lose and lives added. Game is fully working

--- hangman.py
+++ hangman.py
@@ -46,6 +46,17 @@ def get_words_by_difficulty_level(difficulty, countries, capitals):
         return countries + capitals
 
 
+def set_lives_by_difficulty_level(difficulty):
+    total_lives = 7
+    if difficulty == 1:
+        total_lives = 7
+    elif difficulty == 2:
+        total_lives = 5
+    elif difficulty == 3:
+        total_lives = 3
+    return total_lives
+
+
 def get_random_word(word_list):
     return random.choice(word_list)
 
@@ -67,98 +78,45 @@ def word_fully_match(actual_word, letter_set):
     return True
 
 
-def try_to_guess(word_to_guess, found_letters, incorrect_letters):
+def try_to_guess(word_to_guess, found_letters, incorrect_letters, lives):
     print("YOUR WORD")
     display_word(word_to_guess, found_letters)
-    # letter = input("Type a letter - ")
 
     while not word_fully_match(word_to_guess, found_letters):
         letter = input("Type a letter - ")
-        if letter == "quit":
-            exit()
 
-        if len(letter) > 1:
-            print("Please write a letter")
-            break
+        while len(letter) > 1:
+            if letter == "quit":
+                print("Goodbye! Hope to see you in next games :)")
+                exit()
+            letter = input("Please write a letter - ")
 
-        if letter in word_to_guess:
+        if letter.lower() in word_to_guess or letter.upper() in word_to_guess:
             if letter in found_letters:
                 print(f"You found {letter}! Try another letter - ")
             else:
                 found_letters.add(letter.lower())
         elif letter.isnumeric():
-            print(f"Wrong type! Please provide a letter")
+            print("Wrong type! Please provide a letter")
         else:
-            print(f"Wrong guess! Previous wrong letters are:")
-            incorrect_letters.add(letter.lower())
-            print(' '.join(incorrect_letters))
+            if letter.lower() in incorrect_letters:
+                print("It's already wrong.")
+            else:
+                incorrect_letters.add(letter.lower())
+                print("Wrong guess! Previous wrong letters are:")
+                print(' '.join(incorrect_letters))
+                lives -= 1
+                if lives <= 0:
+                    print("YOU LOST THE GAME :(")
+                    exit()
+            print(f"You have {lives} lives left")
 
         display_word(word_to_guess, found_letters)
 
     print("YOU WON THE GAME!! :)")
     display_word(word_to_guess, found_letters)
-
-    # for letter
-    # while letter in found_letters or letter in incorrect_letters:
-    #     if letter in found_letters:
-    #         letter = input(f"You found {letter}! Try another letter")
-    #     if letter in incorrect_letters:
-    #         letter = input(f"You found {letter}! Try another letter")
-
-
-
-
-# PART 1
-# display a menu with at least 3 difficulty choices and ask the user
-# to select the desired level
-
-
-# STEP 2
-# based on the chosen difficulty level, set the values 
-# for the player's lives
-# sample data, normally the word should be chosen from the countries-and-capitals.txt
-# sample data, normally the lives should be chosen based on the difficulty
-
-
-# STEP 3
-# display the chosen word to guess with all letters replaced by "_"
-# for example instead of "Cairo" display "_ _ _ _ _"
-
-
-# STEP 4
-# ask the user to type a letter
-# here you should validate if the typed letter is the word
-# "quit", "Quit", "QUit", "QUIt", "QUIT", "QuIT"... you get the idea :)
-# HINT: use the upper() or lower() built-in Python functions
-
-
-# STEP 5
-# validate if the typed letter is already in the tried letters
-# HINT: search on the internet: `python if letter in list`
-# If it is not, than append to the tried letters
-# If it has already been typed, return to STEP 5. HINT: use a while loop here
-already_tried_letters = [] # this list will contain all the tried letters
-
-
-# STEP 6
-# if the letter is present in the word iterate through all the letters in the variable
-# word_to_guess. If that letter is present in the already_tried_letters then display it,
-# otherwise display "_".
-
-
-# if the letter is not present in the word decrease the value in the lives variable
-# and display a hangman ASCII art. You can search the Internet for "hangman ASCII art",
-# or draw a new beautiful one on your own.
-
-
-
-# STEP 7
-# check if the variable already_tried_letters already contains all the letters necessary
-# to build the value in the variable word_to_guess. If so display a winning message and exit
-# the app.
-# If you still have letters that are not guessed check if you have a non negative amount of lives
-# left. If not print a loosing message and exit the app.
-# If neither of the 2 conditions mentioned above go back to STEP 4
+    print("Goodbye! Hope to see you in next games :)")
+    exit()
 
 
 def main():
@@ -169,10 +127,11 @@ def main():
 
     selected_difficulty = set_the_difficulty()
     words_by_level = get_words_by_difficulty_level(selected_difficulty, countries, capitals)
+    lives = set_lives_by_difficulty_level(selected_difficulty)
+
     word_to_guess = get_random_word(words_by_level)
     print(word_to_guess)
-    try_to_guess(word_to_guess, revealed_letters, wrong_letters)
-    # display_word(word_to_guess)
+    try_to_guess(word_to_guess, revealed_letters, wrong_letters, lives)
 
 
 if __name__ == "__main__":
